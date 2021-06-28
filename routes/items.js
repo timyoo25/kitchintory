f5208410e7380db17366e84f5ebe3a64600a299e
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-06-28 16:49:01 -0400

    updated back end files

--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import * as controllers from '../controllers/items.js'
+import restrict from '../helpers/restrict.js'
 
 const router = Router()
 
-router.get('/items', controllers.getItems)
-router.get('/items/:id', controllers.getItem)
-router.post('/items', controllers.createItem)
-router.put('/items/:id', controllers.updateItem)
-router.delete('/items/:id', controllers.deleteItem)
+router.get('/items', restrict, controllers.getItems)
+router.get('/items/:id', restrict, controllers.getItem)
+router.post('/items', restrict, controllers.createItem)
+router.put('/items/:id', restrict, controllers.updateItem)
+router.delete('/items/:id', restrict, controllers.deleteItem)
 
 export default router
