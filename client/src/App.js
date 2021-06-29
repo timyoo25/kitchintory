e6ee72fcb034e9aa2baf3cec1d93808285f440f0
gchar031 <43050919+gchar031@users.noreply.github.com>
2021-06-29 12:15:51 -0400

    edit tested and completed, passed user to edit from app, and delete yet to be tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
-import { Route, Switch } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { Route, Switch } from "react-router-dom";
+import { useState, useEffect } from "react";
 import ItemDetail from "./screens/ItemDetail/ItemDetail";
-import ItemEdit from "./screens/ItemEdit/ItemEdit"
-import Items from "./screens/Items/Items"
-import SignIn from "./screens/SignIn/SignIn"
-import SignUp from "./screens/SignUp/SignUp"
-import SignOut from "./screens/SignOut/SignOut"
-import './App.css';
-import { verifyUser } from "./services/users"
+import ItemEdit from "./screens/ItemEdit/ItemEdit";
+import Items from "./screens/Items/Items";
+import SignIn from "./screens/SignIn/SignIn";
+import SignUp from "./screens/SignUp/SignUp";
+import SignOut from "./screens/SignOut/SignOut";
+import "./App.css";
+import { verifyUser } from "./services/users";
 
 function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const fetchUser = async () => {
-      const user = await verifyUser()
-      user ? setUser(user) : setUser(null)
-    }
-    fetchUser()
-  }, [])
+      const user = await verifyUser();
+      user ? setUser(user) : setUser(null);
+    };
+    fetchUser();
+  }, []);
 
   return (
     <div className="App">
       <Switch>
-
         <Route exact path="/items">
           <Items user={user} />
         </Route>
@@ -45,13 +44,11 @@ function App() {
         </Route>
 
         <Route exact path="/items/:id/edit">
-          <ItemEdit />
+          <ItemEdit user={user} />
         </Route>
-
       </Switch>
     </div>
   );
 }
 
 export default App;
-
