46696b60749815db0f2a816d72b801769b045845
Will Metivier <willnolin@gmail.com>
2021-07-01 12:28:37 -0400

    footer with github link

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,5 +1,10 @@
 import "./Footer.css";
 
 export default function Footer() {
-  return <div className="footer-parent">Foooooooooooot</div>;
+  return <div className="footer-parent">
+    <a href="https://github.com/timyoo25/kitchintory"
+    className="github-link">
+      <i class="fab fa-github"></i>
+    </a>
+  </div>;
 }
