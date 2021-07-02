5ac8c3d0bbd3b1fc6b30cdffc89f240641f1e34a
Will Metivier <willnolin@gmail.com>
2021-07-02 10:51:43 -0400

    style on item create

--- models/item.js
+++ models/item.js
@@ -6,7 +6,10 @@ const Schema = mongoose.Schema
 const Item = new Schema(
   {
     name: { type: String, required: true },
-    imgURL: { type: String },
+    imgURL: {
+      type: String
+      // default: 'https://res.cloudinary.com/willnolin/ image / upload / v1625236095 / color_basket_utvt7n.png'
+    },
     price: { type: Number, required: true },
     quantity: { type: Number, required: true },
     category: {
